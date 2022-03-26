ba504d9fa4d0df13f5a634af989216ff988183b1
shinjaeseok <joohong2000@gmail.com>
2022-03-26 15:19:45 +0900

    test

--- resources/views/loginPage.blade.php
+++ resources/views/loginPage.blade.php
@@ -27,7 +27,7 @@
     <div class="middle-box text-center loginscreen animated fadeInDown">
         <div>
             <div class="mb-4" style="margin-top:150px;">
-                <h3 class="font-bold" style="color:#000000; font-size: 24px;">그룹웨어 TEST</h3>
+                <h3 class="font-bold" style="color:#000000; font-size: 24px;">그룹웨어</h3>
                 {{--<h3 class="font-bold" style="color:#000000; font-size: 24px;">제이픽스에 오신것을<br> 환영합니다.</h3>--}}
             </div>
             {{--<h3>로그인</h3>--}}
